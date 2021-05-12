7bdefbd61e67cdb206bed60272a19304842859da
Ubuntu <ubuntu@ip-172-31-29-103.ap-northeast-1.compute.internal>
2021-05-12 06:16:37 +0000

    fin chp8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "welcome to the sample app" 
       redirect_to @user
       # redirect_to(user_path(@user))
